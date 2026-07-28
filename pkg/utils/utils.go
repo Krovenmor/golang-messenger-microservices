87907ba612304962fd2560b5024348ad84126189
krovenmor <bossmozgov228@gmail.com>
2026-07-28 17:58:37 +0300

    Added func to get uuid from Auth

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,24 +1,38 @@
 package utils
 
 import (
+	"MyMessenger/pkg/jwt"
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
-func Send[T any](rw http.ResponseWriter, to_send *T) {
-	rw.Header().Set("Content-type", "application/json")
-	rw.WriteHeader(http.StatusOK)
-	err := json.NewEncoder(rw).Encode(*to_send)
+func Send[T any](w http.ResponseWriter, toSend *T) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(toSend)
 	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func Recv[T any](req *http.Request, to_rcv *T) error {
-	err := json.NewDecoder(req.Body).Decode(&to_rcv)
+func Recv[T any](r *http.Request, toRcv *T) error {
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(toRcv)
 	if err != nil {
-		return fmt.Errorf("Not valid JSON: %q", err.Error())
+		return fmt.Errorf("Not valid JSON: %w", err)
 	}
 	return nil
+}
+
+func GetUuidFromContext(r *http.Request) (uuid.UUID, error) {
+	sUserId := r.Context().Value(jwt.UserIdKey).(string)
+	uUserId, err := uuid.Parse(sUserId)
+	if err != nil {
+		return uUserId, fmt.Errorf("GetUuidFromContext(): trouble with parsing uuid: %w", err)
+	}
+	return uUserId, nil
 }
