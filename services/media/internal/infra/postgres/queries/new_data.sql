55175f0b61d1298140dd2004e46dd602bded7b27
krovenmor <bossmozgov228@gmail.com>
2026-08-29 21:55:34 +0300

    fix

--- services/media/internal/infra/postgres/queries/new_data.sql
+++ services/media/internal/infra/postgres/queries/new_data.sql
@@ -2,6 +2,7 @@ with upsert_user as (
     update users
     set space_filled = users.space_filled + $5,
     files_count = users.files_count + 1
+    where user_id = $1
     returning user_id
 )
 insert into usersdata (user_id, media_id, media_type, media_subtype, media_size, is_public)
